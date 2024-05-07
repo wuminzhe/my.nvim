094ac911b2287932ab5c5efbc51bb3c741ba77b0
Aki Wu <wuminzhe@gmail.com>
2024-05-07 18:27:06 +0800

    update

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -189,6 +189,12 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- keymaps by aki
+vim.keymap.set('x', 'p', 'pgvy', { desc = 'Paste multiple times' })
+
+-- set '-' in normal mode to go to the netrw file explorer(call Rex)
+vim.keymap.set('n', '-', '<cmd>Rex<CR>', { desc = 'Open netrw file explorer' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
