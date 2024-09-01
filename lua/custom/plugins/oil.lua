f42b85b04e5321d013f19bcf128631e1b94b37c5
Aki Wu <wuminzhe@gmail.com>
2024-09-01 21:55:04 +0800

    add oil and avanta

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,7 +3,19 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      columns = { 'icon' },
+      keymaps = {
+        ['C-h'] = false,
+        ['M-h'] = 'actions.select_split',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+    -- Open parent directory in floating window
+    vim.keymap.set('n', '<space>-', require('oil').toggle_float)
   end,
 }
